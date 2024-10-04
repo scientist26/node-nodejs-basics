1d145d8fbd1f873a1f71f069d7ba3be992175ff1
Andrei Bezmen <andryshabez26@gmail.com>
2024-10-05 00:07:35 +0200

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const pairs = process.argv.reduce((acc, arg, index) => {
+    if (arg.startsWith('--')) {
+      acc.push({ key: arg.slice(2), value: process.argv[index + 1] });
+    }
+    return acc;
+  }, []);
+
+  pairs.forEach((pair) => console.log(`${pair.key} is ${pair.value}`));
 };
 
 parseArgs();
