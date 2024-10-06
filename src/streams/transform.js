172b7a8fc91399d237b076876bca94961d496199
Andrei Bezmen <andryshabez26@gmail.com>
2024-10-06 17:34:38 +0200

    feat: implement streams  functions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform, pipeline } from 'stream';
+
+const readableFromTerminalStream = process.stdin;
+const writableToTerminalStream = process.stdout;
+
 const transform = async () => {
-    // Write your code here 
+  const reverseText = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedText = chunk.toString().trim().split('').reverse().join('');
+      callback(null, reversedText + '\n');
+    },
+  });
+  //   readableFromTerminalStream.pipe(reverseText).pipe(writableToTerminalStream);
+  pipeline(readableFromTerminalStream, reverseText, writableToTerminalStream, (err) => {
+    console.error('Pipeline failed.', err);
+  });
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+
+const readableFromTerminalStream = process.stdin;
+const writableToFileStream = fs.createWriteStream('src/streams/files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  readableFromTerminalStream.pipe(writableToFileStream);
+  readableFromTerminalStream.resume();
+  //   readableFromTerminalStream.on('data', (chunk) => {
+  //     // close stream if input is an empty
+  //     if (chunk.toString().length === 1) {
+  //       readableFromTerminalStream.unpipe(writableToFileStream);
+  //     }
+  //   });
 };
 
 await write();
